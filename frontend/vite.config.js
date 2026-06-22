8cc6022ef933f7df235b129b50c0b8b67836360c
Joshua <sorochijoshua30@gmail.com>
2026-06-22 17:08:09 +0100

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
-  }
+  },
 })
